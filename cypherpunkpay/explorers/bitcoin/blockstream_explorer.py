a8e0b0724598d296b4f9d3c475b6f0ba9358f6a7
cypherpunkdev <>
2021-11-18 12:56:52 +0100

    Fix Blockstream explorer as they migrated their onion API to v3

--- cypherpunkpay/explorers/bitcoin/blockstream_explorer.py
+++ cypherpunkpay/explorers/bitcoin/blockstream_explorer.py
@@ -6,11 +6,11 @@ class BlockstreamExplorer(AbsEsploraExplorer):
     def api_endpoint(self) -> str:
         if self.mainnet():
             if self.use_tor():
-                return 'http://explorernuoc63nb.onion/api'
+                return 'http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/api'
             else:
                 return 'https://blockstream.info/api'
         else:
             if self.use_tor():
-                return 'http://explorernuoc63nb.onion/testnet/api'
+                return 'http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion//testnet/api'
             else:
                 return 'https://blockstream.info/testnet/api'
