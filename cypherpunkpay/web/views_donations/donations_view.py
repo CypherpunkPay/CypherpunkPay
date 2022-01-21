55649049debf69e87c6555ea101e9537b9f47004
cypherpunkdev <>
2022-01-21 15:18:24 +0000

    Add alternative "entertainment" theme for donations home screen making the support now complete

--- cypherpunkpay/web/views_donations/donations_view.py
+++ cypherpunkpay/web/views_donations/donations_view.py
@@ -11,10 +11,11 @@ class DonationsViews(BaseView):
     def __init__(self, request):
         self.request = request
 
-    @view_config(route_name='get_donations', renderer='web/html/donations/index.jinja2')
+    @view_config(route_name='get_donations', renderer='jinja2')
     def get_donations(self):
         wait_a_sec = self.request.params.get('wait_a_sec', None) == 'true' and not App().is_fully_initialized()
         errors = json.loads(self.request.params.get('errors', '{}'))
+        self.request.override_renderer = f'web/html/donations/theme_{self.theme()}/index.jinja2'
         return {
             'title': f'Donate{self.donations_cause_text()}',
             'donations_cause': self.app_config().donations_cause(),
