013ac74343d346a1a4d339fd7d127962237cb68c
cypherpunkdev <>
2022-03-15 11:07:43 +0000

    Change address_credits_test to pytest assert

--- test/unit/models/address_credits_test.py
+++ test/unit/models/address_credits_test.py
@@ -13,7 +13,7 @@ class AddressCreditsTest(CypherpunkpayTestCase):
             Credit(1, None, True),
             Credit(1, None, False)
         ], 1)
-        self.assertEqual(4, len(account_credits.all()))
+        assert len(account_credits.all()) == 4
 
     def test_unconfirmed_replaceable(self):
         account_credits = AddressCredits([
@@ -23,7 +23,7 @@ class AddressCreditsTest(CypherpunkpayTestCase):
             Credit(50, confirmed_height=None, has_replaceable_flag=True),
             Credit(1, confirmed_height=None, has_replaceable_flag=False)
         ], 1)
-        self.assertEqual(2, len(account_credits.unconfirmed_replaceable()))
+        assert len(account_credits.unconfirmed_replaceable()) == 2
 
     def test_unconfirmed_non_replaceable(self):
         account_credits = AddressCredits([
@@ -33,7 +33,7 @@ class AddressCreditsTest(CypherpunkpayTestCase):
             Credit(1, confirmed_height=None, has_replaceable_flag=False),
             Credit(50, confirmed_height=None, has_replaceable_flag=False)
         ], 1)
-        self.assertEqual(2, len(account_credits.unconfirmed_non_replaceable()))
+        assert len(account_credits.unconfirmed_non_replaceable()) == 2
 
     def test_confirmed_1(self):
         account_credits = AddressCredits([
@@ -43,7 +43,7 @@ class AddressCreditsTest(CypherpunkpayTestCase):
             Credit(1, confirmed_height=None, has_replaceable_flag=True),
             Credit(1, confirmed_height=None, has_replaceable_flag=False)
         ], 1002)
-        self.assertEqual(3, len(account_credits.confirmed_1()))
+        assert len(account_credits.confirmed_1()) == 3
 
     def test_confirmed_n(self):
         account_credits = AddressCredits([
@@ -53,41 +53,41 @@ class AddressCreditsTest(CypherpunkpayTestCase):
             Credit(1, confirmed_height=None, has_replaceable_flag=True),
             Credit(1, confirmed_height=None, has_replaceable_flag=False)
         ], 1002)
-        self.assertEqual(2, len(account_credits.confirmed_n(2)))
+        assert len(account_credits.confirmed_n(2)) == 2
 
     def test_equal(self):
         # equal / empty
-        self.assertTrue(AddressCredits([], 0) == AddressCredits([], 0))
+        assert AddressCredits([], 0) == AddressCredits([], 0)
 
         # equal / same single Credit
         ac1 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
         ac2 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
-        self.assertTrue(ac1 == ac2)
+        assert ac1 == ac2
 
         # equal / same two Credits but different order
         ac1 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True), Credit(2, confirmed_height=1000, has_replaceable_flag=True)], 900)
         ac2 = AddressCredits([Credit(2, confirmed_height=1000, has_replaceable_flag=True), Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
-        self.assertTrue(ac1 == ac2)
+        assert ac1 == ac2
 
         # different size
         ac1 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
         ac2 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True), Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
-        self.assertTrue(ac1 != ac2)
+        assert ac1 != ac2
 
         # different block height
         ac1 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 901)
         ac2 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
-        self.assertTrue(ac1 != ac2)
+        assert ac1 != ac2
 
         # different amount
         ac1 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
         ac2 = AddressCredits([Credit(2, confirmed_height=1000, has_replaceable_flag=True)], 900)
-        self.assertTrue(ac1 != ac2)
+        assert ac1 != ac2
 
         # different confirmed height
         ac1 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
         ac2 = AddressCredits([Credit(1, confirmed_height=1001, has_replaceable_flag=True)], 900)
-        self.assertTrue(ac1 != ac2)
+        assert ac1 != ac2
 
         # # different RBF flag
         # ac1 = AddressCredits([Credit(1, confirmed_height=1000, has_replaceable_flag=True)], 900)
