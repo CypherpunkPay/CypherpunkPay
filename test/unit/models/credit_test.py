c4c453922294511b4bcfe21974023d0edbceff8d
cypherpunkdev <>
2022-03-16 11:04:31 +0000

    Change credit_test to pytest assert

--- test/unit/models/credit_test.py
+++ test/unit/models/credit_test.py
@@ -6,22 +6,22 @@ class CreditTest(CypherpunkpayTestCase):
 
     def test_is_unconfirmed_replaceable(self):
         credit = Credit(1, confirmed_height=None, has_replaceable_flag=True)
-        self.assertTrue(credit.is_unconfirmed_replaceable())
+        assert credit.is_unconfirmed_replaceable()
 
     def test_is_unconfirmed_non_replaceable(self):
         credit = Credit(1, confirmed_height=None, has_replaceable_flag=False)
-        self.assertTrue(credit.is_unconfirmed_non_replaceable())
+        assert credit.is_unconfirmed_non_replaceable()
 
     def test_is_unconfirmed(self):
         credit = Credit(1, confirmed_height=None, has_replaceable_flag=True)
-        self.assertTrue(credit.is_unconfirmed())
+        assert credit.is_unconfirmed()
 
         credit = Credit(1, confirmed_height=None, has_replaceable_flag=False)
-        self.assertTrue(credit.is_unconfirmed())
+        assert credit.is_unconfirmed()
 
     def test_is_confirmed(self):
         credit = Credit(1, confirmed_height=1, has_replaceable_flag=True)
-        self.assertTrue(credit.is_confirmed())
+        assert credit.is_confirmed()
 
         credit = Credit(1, confirmed_height=1, has_replaceable_flag=False)
-        self.assertTrue(credit.is_confirmed())
+        assert credit.is_confirmed()
