f10275c1ec78e71f3828de3f259b26adcdd48604
cypherpunkdev <>
2022-03-16 11:03:22 +0000

    Change charge_test to pytest assert

--- test/unit/models/charge_test.py
+++ test/unit/models/charge_test.py
@@ -12,39 +12,39 @@ class ChargeTest(CypherpunkpayTestCase):
     def test_instantiates(self):
         charge = Charge(total=Decimal('100.97'), currency='USD', time_to_pay_ms=1000, time_to_complete_ms=10_000)
 
-        self.assertTrue(charge.uid)
-        self.assertTrue(charge.created_at)
+        assert charge.uid
+        assert charge.created_at
 
-        self.assertEqual(charge.total, Decimal('100.97'))
-        self.assertEqual(charge.currency, 'usd')
+        assert charge.total == Decimal('100.97')
+        assert charge.currency == 'usd'
 
-        self.assertEqual(charge.cc_received_total, 0)
-        self.assertEqual(charge.pay_status, 'unpaid')
-        self.assertEqual(charge.status, 'draft')
+        assert charge.cc_received_total == 0
+        assert charge.pay_status == 'unpaid'
+        assert charge.status == 'draft'
 
     def test_charge_description(self):
         # Donations
         charge = ExampleCharge.create(beneficiary=None, what_for=None, merchant_order_id=None)
-        self.assertEqual(charge.description, 'Donation')
+        assert charge.description == 'Donation'
 
         charge = ExampleCharge.create(beneficiary='WikiLeaks', what_for=None, merchant_order_id=None)
-        self.assertEqual(charge.description, 'Donation to WikiLeaks')
+        assert charge.description == 'Donation to WikiLeaks'
 
         charge = ExampleCharge.create(beneficiary=None, what_for='Free Assange', merchant_order_id=None)
-        self.assertEqual(charge.description, 'Donation to Free Assange')
+        assert charge.description == 'Donation to Free Assange'
 
         charge = ExampleCharge.create(beneficiary='WikiLeaks', what_for='Free Assange', merchant_order_id=None)
-        self.assertEqual(charge.description, 'Donation to WikiLeaks, Free Assange')
+        assert charge.description == 'Donation to WikiLeaks, Free Assange'
 
         # Merchant
         charge = ExampleCharge.create(beneficiary=None, what_for=None, merchant_order_id='437')
-        self.assertMatch('Order 437, charge ', charge.description)
+        assert 'Order 437, charge ' in charge.description
 
         charge = ExampleCharge.create(beneficiary='Tesla', what_for=None, merchant_order_id='437')
-        self.assertMatch('Tesla, order 437, charge ', charge.description)
+        assert 'Tesla, order 437, charge ' in charge.description
 
         charge = ExampleCharge.create(beneficiary=None, what_for='Model S', merchant_order_id='437')
-        self.assertMatch('Model S, charge ', charge.description)
+        assert 'Model S, charge ' in charge.description
 
         charge = ExampleCharge.create(beneficiary='Tesla', what_for='Model S', merchant_order_id='437')
-        self.assertMatch('Tesla, Model S, charge ', charge.description)
+        assert 'Tesla, Model S, charge ' in charge.description
