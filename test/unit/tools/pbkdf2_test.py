320614f450723d23fa155c358480e635ccce644b
cypherpunkdev <>
2022-03-16 11:19:53 +0000

    Change pbkdf2_test to pytest assert

--- test/unit/tools/pbkdf2_test.py
+++ test/unit/tools/pbkdf2_test.py
@@ -12,11 +12,11 @@ class PBKDF2Test(CypherpunkpayTestCase):
         h = PBKDF2.hash('password123')
 
         # proper length
-        self.assertEqual(len(h), 8 + 64 + 64)
+        assert len(h) == 8 + 64 + 64
 
         # million iterations
         MILLION_ITERATIONS = '00030d40'  # unsigned 4 bytes
-        self.assertTrue(MILLION_ITERATIONS in h)
+        assert MILLION_ITERATIONS in h
 
     # If this starts failing then maybe the default 12 rounds is no longer enough
     def test_hash_speed(self):
@@ -24,10 +24,10 @@ class PBKDF2Test(CypherpunkpayTestCase):
         start = time.time()
         PBKDF2.hash('password123')
         end = time.time()
-        self.assertGreater(end-start, 0.05)
+        assert end-start > 0.05
 
     # Based on https://bcrypt-generator.com/
     def test_password_is_correct(self):
-        self.assertTrue(PBKDF2.password_is_correct('', PBKDF2.hash('')))
-        self.assertTrue(PBKDF2.password_is_correct('password123', PBKDF2.hash('password123')))
-        self.assertTrue(PBKDF2.password_is_correct('QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*(_+-={}|[];:,.<>', PBKDF2.hash('QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*(_+-={}|[];:,.<>')))
+        assert PBKDF2.password_is_correct('', PBKDF2.hash(''))
+        assert PBKDF2.password_is_correct('password123', PBKDF2.hash('password123'))
+        assert PBKDF2.password_is_correct('QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*(_+-={}|[];:,.<>', PBKDF2.hash('QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*(_+-={}|[];:,.<>'))
