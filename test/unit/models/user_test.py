b830be8d22f0e99c08301ad500d30572025446b5
cypherpunkdev <>
2022-03-16 11:07:49 +0000

    Change user_test to pytest assert

--- test/unit/models/user_test.py
+++ test/unit/models/user_test.py
@@ -6,6 +6,6 @@ class UserTest(CypherpunkpayTestCase):
 
     def test_instantiates(self):
         user = User(username='satoshi', password_hash='password hash')
-        self.assertEqual(user.username, 'satoshi')
-        self.assertEqual(user.password_hash, 'password hash')
-        self.assertIsNotNone(user.created_at)
+        assert user.username == 'satoshi'
+        assert user.password_hash == 'password hash'
+        assert user.created_at is not None
