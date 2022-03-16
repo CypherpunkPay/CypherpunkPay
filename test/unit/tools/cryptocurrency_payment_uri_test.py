656a8f551d64a0adcf7263b6a117f7121e299bb8
cypherpunkdev <>
2022-03-16 11:07:49 +0000

    Change cryptocurrency_payment_uri_test to pytest assert

--- test/unit/tools/cryptocurrency_payment_uri_test.py
+++ test/unit/tools/cryptocurrency_payment_uri_test.py
@@ -9,15 +9,15 @@ class CryptocurrencyPaymentUriTest(CypherpunkpayTestCase):
     def test_get_btc(self):
         # Address
         uri = CryptocurrencyPaymentUri.get('btc', '175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W')
-        self.assertEqual('bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W', uri)
+        assert uri == 'bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W'
 
         # Address, amount
         uri = CryptocurrencyPaymentUri.get('btc', 'mgwm4YLsnPaSPfnQ5kdm3URz7ejXKBGxbV', amount=Decimal('0.00208907'))
-        self.assertEqual('bitcoin:mgwm4YLsnPaSPfnQ5kdm3URz7ejXKBGxbV?amount=0.00208907', uri)
+        assert uri == 'bitcoin:mgwm4YLsnPaSPfnQ5kdm3URz7ejXKBGxbV?amount=0.00208907'
 
         # Address, amount, label, message
         uri = CryptocurrencyPaymentUri.get('btc', '175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W', amount=Decimal('50'), label='Luke-Jr', message='Donation for project xyz')
-        self.assertEqual('bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=50&label=Luke-Jr&message=Donation%20for%20project%20xyz', uri)
+        assert uri == 'bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=50&label=Luke-Jr&message=Donation%20for%20project%20xyz'
 
     def test_get_xmr(self):
         # Address
@@ -25,7 +25,7 @@ class CryptocurrencyPaymentUriTest(CypherpunkpayTestCase):
             'xmr',
             '46BeWrHpwXmHDpDEUmZBWZfoQpdc6HaERCNmx1pEYL2rAcuwufPN9rXHHtyUA4QVy66qeFQkn6sfK8aHYjA3jk3o1Bv16em'
         )
-        self.assertEqual('monero:46BeWrHpwXmHDpDEUmZBWZfoQpdc6HaERCNmx1pEYL2rAcuwufPN9rXHHtyUA4QVy66qeFQkn6sfK8aHYjA3jk3o1Bv16em', uri)
+        assert uri == 'monero:46BeWrHpwXmHDpDEUmZBWZfoQpdc6HaERCNmx1pEYL2rAcuwufPN9rXHHtyUA4QVy66qeFQkn6sfK8aHYjA3jk3o1Bv16em'
 
         # Address, amount
         uri = CryptocurrencyPaymentUri.get(
@@ -33,7 +33,7 @@ class CryptocurrencyPaymentUriTest(CypherpunkpayTestCase):
             '46BeWrHpwXmHDpDEUmZBWZfoQpdc6HaERCNmx1pEYL2rAcuwufPN9rXHHtyUA4QVy66qeFQkn6sfK8aHYjA3jk3o1Bv16em',
             amount=Decimal('239.390146789012')
         )
-        self.assertEqual('monero:46BeWrHpwXmHDpDEUmZBWZfoQpdc6HaERCNmx1pEYL2rAcuwufPN9rXHHtyUA4QVy66qeFQkn6sfK8aHYjA3jk3o1Bv16em?tx_amount=239.390146789012', uri)
+        assert uri == 'monero:46BeWrHpwXmHDpDEUmZBWZfoQpdc6HaERCNmx1pEYL2rAcuwufPN9rXHHtyUA4QVy66qeFQkn6sfK8aHYjA3jk3o1Bv16em?tx_amount=239.390146789012'
 
         # Address, amount, label, message
         uri = CryptocurrencyPaymentUri.get(
@@ -43,4 +43,4 @@ class CryptocurrencyPaymentUriTest(CypherpunkpayTestCase):
             label='Luke-Jr',
             message='Donation for project xyz'
         )
-        self.assertEqual('monero:46BeWrHpwXmHDpDEUmZBWZfoQpdc6HaERCNmx1pEYL2rAcuwufPN9rXHHtyUA4QVy66qeFQkn6sfK8aHYjA3jk3o1Bv16em?tx_amount=239.390146789012&recipient_name=Luke-Jr&tx_description=Donation%20for%20project%20xyz', uri)
+        assert uri == 'monero:46BeWrHpwXmHDpDEUmZBWZfoQpdc6HaERCNmx1pEYL2rAcuwufPN9rXHHtyUA4QVy66qeFQkn6sfK8aHYjA3jk3o1Bv16em?tx_amount=239.390146789012&recipient_name=Luke-Jr&tx_description=Donation%20for%20project%20xyz'
